bf5ef224bc736e352a601264b3e5670f63786737
lvaudor <lise.vaudor@ens-lyon.fr>
2020-10-15 17:03:28 +0200

    add escape %

--- R/format_time_for_qtvar.R
+++ R/format_time_for_qtvar.R
@@ -1,4 +1,4 @@
-#' Format time to be formatted as banqueHydro expects for QTVAR ("%d/%m/%Y %H:%M")
+#' Format time to be formatted as banqueHydro expects for QTVAR ("\%d/\%m/\%Y \%H:\%M")
 #' @param mytime time
 #' @return time in format "%d/%m/%Y %H:%M"
 #' @export
